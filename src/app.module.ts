79892317e88ff1aade0962a6b797f2ae86fd8c20
Nwogu Chidera <chideranwogu2003@gmail.com>
2026-03-16 14:04:24 +0100

    feat: added core app modules, auto imported in the app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WalletModule } from './wallet/wallet.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { FxModule } from './fx/fx.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, WalletModule, TransactionsModule, FxModule],
   controllers: [AppController],
   providers: [AppService],
 })
